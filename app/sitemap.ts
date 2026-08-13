fca3d5aabfccd6053881228b0d13232b053908dd
malik taylor l-mint <oscarnyamosi1@gmail.com>
2026-08-13 17:37:55 +0300

    changed sitemap url to benkiz.org from events.benkiz.org

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,37 +3,35 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://events.benkiz.org',
-      lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://events.benkiz.org/about',
+      url: 'https://benkiz.org/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.9,
     },
     {
-      url: 'https://events.benkiz.org/contact',
+      url: 'https://benkiz.org/contact',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://events.benkiz.org/menu',
+      url: 'https://benkiz.org/menu',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://events.benkiz.org/services',
+      url: 'https://benkiz.org/services',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://events.benkiz.org/blog',
+      url: 'https://benkiz.org/blog',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
