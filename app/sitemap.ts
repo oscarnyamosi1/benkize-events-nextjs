f3e0d4719fddc28e48393aed74474a2e62c80cdf
malik taylor l-mint <oscarnyamosi1@gmail.com>
2026-08-15 15:12:08 +0300

    fixed sitemap url issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,8 @@ import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
-    {
+    {url: 'https://benkiz.org/',
+      lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
